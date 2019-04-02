420522affbc4d84aaa4e2b920e77997ee8a02772
AnastasiaKremenetskaya <46082850+AnastasiaKremenetskaya@users.noreply.github.com>
2019-04-02 18:18:49 +0400

    Commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-heade>r">
           <img src={pic} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
